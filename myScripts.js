15515048ec3edf5c7f24529b3a23de2864874d97
CartWheelSmith <61665175+CartWheelSmith@users.noreply.github.com>
2023-11-08 17:51:07 +0200

    Update Commit

--- myScripts.js
+++ myScripts.js
@@ -1,11 +1,3 @@
-$(document).ready(function(e){
-    if(window.location.href.includes("shorts")){
-        chrome.tabs.update({url: "http://www.youtube.com"});
-        console.log("redirect here!")
-    }
-});
-
-
 function removeShortsElements(){
 
     //Removes Youtube shorts container on Main page.
