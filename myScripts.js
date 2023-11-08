6647f92ce3a2e573bd7349e0f77115062b4ca4df
CartWheelSmith <61665175+CartWheelSmith@users.noreply.github.com>
2023-11-08 17:50:56 +0200

    Update Commit

--- myScripts.js
+++ myScripts.js
@@ -3,5 +3,23 @@ $(document).ready(function(e){
         chrome.tabs.update({url: "http://www.youtube.com"});
         console.log("redirect here!")
     }
+});
 
+
+function removeShortsElements(){
+
+    //Removes Youtube shorts container on Main page.
+    const shortsContainer = $('ytd-rich-section-renderer'); //I am not sure if there is a better way find the YouTube Shorts on the home page as there might be other containers that are not Shorts specific using the same element.
+    if(shortsContainer){
+        shortsContainer.remove();
+    }
+    const shortsIcon = $('ytd-mini-guide-entry-renderer[aria-label="Shorts"]');
+    if(shortsIcon){
+        shortsIcon.remove();
+    }
+}
+
+//Remove elements once page loads.
+$(document).ready(function(){
+    removeShortsElements();
 });
