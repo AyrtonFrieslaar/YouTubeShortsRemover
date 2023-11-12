7b2e2d0f4d049646979678e272f2ad498bbab1cd
CartWheelSmith <61665175+CartWheelSmith@users.noreply.github.com>
2023-11-12 22:09:38 +0200

    Update - Removed debug code

--- background.js
+++ background.js
@@ -3,11 +3,8 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo){
     if(changeInfo.url.includes("/shorts/")){//The function should only be called if the url of the tab matches */shorts/*
         
         chrome.tabs.goBack(tabId).then(response => {// I'm not sure if a variable that is not being used is nescessary for the short hand function in this promise catch
-            console.log("go back here!"); //debug
         }, nonresponse => {//if promise is unsuccessful
             //I am uncertain on the correct approach here.
-            
-            console.log("redirect to home here!"); //debug
             //The current tab will be redirected.
             chrome.tabs.update({url: "http://www.youtube.com"});
         });                
--- myScripts.js
+++ myScripts.js
@@ -1,25 +1,20 @@
 function removeShortsElements(){
-    console.log("This function runs!");
     //Removes Youtube shorts container on Main page.
     const shortsContainer = $('ytd-rich-section-renderer'); //I am not sure if there is a better way find the YouTube Shorts on the home page as there might be other containers that are not Shorts specific using the same element.
     const shortReelContainer = $('ytd-reel-shelf-renderer'); //Seems like the only element that contains Shorts when searching.
     if(shortsContainer.length || shortReelContainer.length){
         shortsContainer.remove();
         shortReelContainer.remove();//I should remove this in its own condition.
-
-        console.log("removed containers here!");//debug
     }
-    //I swear this attributes in this element changed as I was working on this.
+
     const shortsIcon = $('yt-formatted-string:contains(Shorts)');
     if(shortsIcon.text() == "Shorts"){
         shortsIcon.parents().eq(1).remove();//According to my understanding, only the element referrenced in the if condition parents 2 levels up will be removed
-        console.log("removed icon here!");//debug
     }
 }
 //Remove elements once page loads.
 $(document).ready(function(){
     removeShortsElements();
-    console.log("Doc ready!");//debug
 });
 //At this point I realise I need to somehow dynamically remove containers
 //Found the answer from someone who has already done this. 
